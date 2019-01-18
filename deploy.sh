f3e7d6e6189283b2d7cf0b5dee99585e225f714d
zamarriv <ivan.zamarripa@utc.com>
2019-01-18 11:56:11 -0500

    Adding extra steps to commit the image locally

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ docker run -ti --privileged --name debian -d -p 2222:22 debian:latest sleep 1000
 docker run -ti --privileged --name centos -d -p 2223:22 centos:latest sleep 1000
 
 ansible-playbook install-from-scm.yml
-ansible-playbook -i containers playbook.yml
+ansible-playbook -vvvv -i containers playbook.yml
